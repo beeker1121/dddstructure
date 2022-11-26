18f143f2cfc428ac4e6c8490acf6b2c7bf1dad33
Conner Hewitt <connerh87@gmail.com>
2022-11-26 15:51:08 -0600

    Updated billingtwo program

--- cmd/billingtwo/main.go
+++ cmd/billingtwo/main.go
@@ -27,46 +27,24 @@ func main() {
 	fmt.Println("[+] Creating new service...")
 	serv := service.New(coreserv)
 
-	/* --- Create and get merchant --- */
+	/* --- Create merchant and add amount due --- */
 
-	// Create a new merchant.
-	fmt.Println("[+] Creating a merchant via the service...")
-	m, err := serv.Merchant.Create(&merchant.CreateParams{
-		ID:    1,
-		Name:  "John Doe",
-		Email: "johndoe@gmail.com",
-	})
-	if err != nil {
+	// Create new merchant and add amount due.
+	if err := createMerchantAndAmountDue(serv); err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("[+] Created merchant: %+v\n", *m)
-
-	// Get the merchant.
-	fmt.Println("[+] Getting merchant via the service...")
-	m, err = serv.Merchant.GetByID(1)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("[+] Got merchant: %+v\n", *m)
 
 	/* --- Handle billing --- */
 
-	// Get the user for this merchant.
-	fmt.Println("[+] Getting user for merchant...")
-	u, err := serv.User.GetByID(1)
+	// Getting accounting entry for the merchant we created.
+	fmt.Printf("[+] Getting accounting entry for merchant '%v'...\n", 1)
+	a, err := serv.Accounting.GetByID(1)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("[+] Got user: %+v\n", *u)
+	fmt.Printf("[+] Got accounting entry: %+v\n", *a)
 
-	// Add amount owed via billing service.
-	fmt.Println("[+] Adding amount owed via billing service...")
-	if err := serv.Billing.AddAmountDue(m.ID, u.ID, 100); err != nil {
-		panic(err)
-	}
-
-	// Get billing information for all merchants.
+	// Handle billing information for all merchants.
 	fmt.Println("[+] Handling billing for all merchants...")
 	err = serv.Billing.HandleMerchantBilling()
 	if err != nil {
@@ -74,10 +52,49 @@ func main() {
 	}
 
 	// Getting accounting entry for the merchant we created.
-	fmt.Printf("[+] Getting accounting entry for merchant '%v'...\n", m.ID)
-	a, err := serv.Accounting.GetByID(1)
+	fmt.Printf("[+] Getting accounting entry for merchant '%v'...\n", 1)
+	a, err = serv.Accounting.GetByID(1)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("[+] Got accounting entry: %+v\n", *a)
+}
+
+func createMerchantAndAmountDue(s *service.Service) error {
+	// Create a new merchant.
+	fmt.Println("[+] Creating a merchant via the service...")
+	m, err := s.Merchant.Create(&merchant.CreateParams{
+		ID:    1,
+		Name:  "John Doe",
+		Email: "johndoe@gmail.com",
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("[+] Created merchant: %+v\n", *m)
+
+	// Get the merchant.
+	fmt.Println("[+] Getting merchant via the service...")
+	m, err = s.Merchant.GetByID(1)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("[+] Got merchant: %+v\n", *m)
+
+	// Get the user for this merchant.
+	fmt.Println("[+] Getting user for merchant...")
+	u, err := s.User.GetByID(1)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("[+] Got user: %+v\n", *u)
+
+	// Add amount owed via billing service.
+	fmt.Println("[+] Adding amount owed via billing service...")
+	if err := s.Billing.AddAmountDue(m.ID, u.ID, 100); err != nil {
+		return err
+	}
+
+	return nil
 }
